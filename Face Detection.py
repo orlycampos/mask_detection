c1f8755646baae5c3b364e61e1190ba352b7d100
Orlando Campos <camposo@camposo-a01.vmware.com>
2020-08-09 18:42:01 -0600

    Changed the library used for the prediction

--- Face Detection.py
+++ Face Detection.py
@@ -2,10 +2,13 @@ import cv2
 import logging as log
 import datetime as dt
 from time import sleep
-from keras.models import load_model
-from pandas import np
+from imageai.Prediction.Custom import CustomImagePrediction
 
-model = load_model("/Users/camposo/Desktop/Camera Testing/model_ex-002_acc-0.703598.h5")
+prediction = CustomImagePrediction()
+prediction.setModelTypeAsResNet()
+prediction.setModelPath("model_ex-046_acc-0.967188.h5")
+prediction.setJsonPath("model_class.json")
+prediction.loadModel(num_objects=3)
 
 def face_detection():
     faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -29,30 +32,32 @@ def face_detection():
             gray,
             scaleFactor=1.1,
             minNeighbors=5,
-            minSize=(30, 30)
+            minSize=(120, 120)
         )
 
         # Draw a rectangle around the faces
         for (x, y, w, h) in faces:
+
             cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            result = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+            result = frame[y: y + h,
+                            x: x + w]
 
             if len(result) > 0:
                 image = result
                 image_resize = cv2.resize(image, (224, 224))
-                image_reshape = np.reshape(image_resize, [1, 224, 224, 3])
-                prediction = model.predict(image_reshape)
-                print(prediction)
-
+                predicted_result = prediction.predictImage(image_resize, result_count=3, input_type="array")
+                color = (0, 255, 0)
+                predicted_data = str(predicted_result[0][0]) + " " + str(predicted_result[1][0])
+                cv2.putText(frame, predicted_data, (x, y - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 
         if anterior != len(faces):
             anterior = len(faces)
             log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
-
         # Display the resulting frame
         cv2.imshow('Video', frame)
-
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
